424ca52b4c261ffc630ad1346f7a885326887a22
mavoo-a11y <marvinlone456@gmail.com>
2025-04-17 19:42:52 +0300

    dimesions.py

---
+++ dimesions.py
@@ -0,0 +1,11 @@
+fruits = ["apple", "banana", "cherry"]
+vegetables = ["carrot", "broccoli", "spinach"]
+cereals = ["rice","kamande", "wheat"]
+
+groceries = [fruits,vegetables,cereals]
+
+print(groceries[2])
+for collection in groceries:
+    for food in collection:
+        print (food,end=" ")
+        print()

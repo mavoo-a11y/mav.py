64f65747145930bb137f8a2823021d03524834a5
mavoo-a11y <marvinlone456@gmail.com>
2025-04-22 19:12:06 +0300

    dictionary.py

---
+++ dictionary.py
@@ -0,0 +1,12 @@
+capitals = {"USA": "Washington D.C.", "France": "Paris", "Germany": "Berlin"}
+
+# if capitals.get("USA"):
+#     print("That capital exists")
+# else :
+#     print("Capital doesn't exist")
+
+keys = capitals.keys()
+
+for key in capitals.keys():
+        print(key)
+   

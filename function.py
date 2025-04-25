00947bfad27e5f6f4dfbf3f61a3ba86f4d91d416
mavoo-a11y <marvinlone456@gmail.com>
2025-04-25 19:50:46 +0300

    function.py

---
+++ function.py
@@ -0,0 +1,20 @@
+# def happy_birthday(name):
+#     print(f"Happy Birthday to {name}!")
+#     print("You are now getting old!")
+#     print("Happy Birthday to you Siz!")
+#     print("We are all happy for you and we love you!")
+#     print("We wish you the best in life!")
+#     print()
+
+# happy_birthday("Pendo")
+          
+def create_name(first, last):
+    first = first.capitalize()
+    last = last.capitalize()
+    return first + " " + last
+
+full_name = create_name("Pendo", "Mwanza")
+
+print(full_name)
+    
+    

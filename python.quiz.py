361b32b518880c13609df5eaeae921864c05eba6
mavoo-a11y <marvinlone456@gmail.com>
2025-05-10 21:07:31 +0300

    python.quiz.py

--- python.quiz.py
+++ python.quiz.py
@@ -18,28 +18,26 @@ question_num = 0
 for question in questions:
    print("-----------------------")
    print(question)
-   for option in options[question_num]: 
-     print(option)
+   for option in options[question_num]:
+       print(option)
 
-     guess = input("Enter (A, B, C, D): ").upper()
-     guesses.append(guess)
-     if guess == answers[question_num]:
+   guess = input("Enter (A, B, C, D): ").upper()
+   guesses.append(guess)
+   if guess == answers[question_num]:
         score += 1
         print("CORRECT!")
-     else:
+   else:
         print("WRONG!")
         print(f"{answers[question_num]} is the correct answer ")
-question_num += 1
+   question_num += 1
 
-print("answers: ", end=" ")
+# print("Correct answers: ", end= " ")
+
 for answer in answers:
-   print(answer, end=" ")
-   print()
-   
-   print("guesses: ", end=" ")
-for guess in guesses:
-   print(guess, end=" ")
-   print()
-   
-score = int(score / len(questions) * 100)
-print (f"Your score is {score}%")
+                print(answer, end=" ")
+                print()
+                print("guesses: ", end=" ")
+
+             
+score_percent = int(score / len(questions) * 100)
+print (f"Your score is {score_percent}%")
